7a22a1eae4a3aa1f3e9cc8658b0327897324c282
EliecerSH <elie.salgado@duocuc.cl>
2024-05-18 05:32:27 -0400

    Primera subida

--- 2.2.2.Bienvenido.py
+++ 2.2.2.Bienvenido.py
@@ -1,3 +1,3 @@
 print(" Bienvenido al mundo de la programación ")
 nom=input(" Para comenzar, Ingresa tu nombre ")
-print(" Bienvenido",nom)
+print(f" Bienvenido {nom}")
--- Resolucio.py
+++
@@ -1,4 +0,0 @@
-valor1=input(" ingresa un valor ")
-numero1=float(valor1)
-resolucion=((numero1*numero1)+(3*numero1)+1)/4
-print("el resultado es",resolucion)
